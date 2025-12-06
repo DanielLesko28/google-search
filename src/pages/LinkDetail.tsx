6301d4d954197a4725e7c858d1d6bd7e184d1565
Daniel <daniel.lesko28@gmail.com>
2025-12-06 13:25:12 +0100

    this works

--- src/pages/LinkDetail.tsx
+++ src/pages/LinkDetail.tsx
@@ -10,21 +10,28 @@ const LinkDetail = () => {
   console.log("detail item", item);
 
   return (
-    <div className="p-4">
-      <button
-        onClick={() => navigate(-1)}
-        className="mb-4 px-4 py-2 bg-gray-400 rounded hover:bg-gray-500"
-      >
-        ← Back
-      </button>
-      <h1 className="text-xl font-bold">{item.title}</h1>
-      <p>
-        <a href={item.link} target="_blank" className="text-blue-600 underline">
-          Visit Link
-        </a>
-      </p>
-      <p className="mt-4">{item.snippet}</p>
-    </div>
+    <section className="w-full flex justify-center">
+      <div className="p-4 w-full max-w-[800px]">
+        <button
+          onClick={() => navigate(-1)}
+          className="my-8 mb-12 px-4 py-2 text-md bg-amber-500 rounded hover:bg-gray-500"
+        >
+          ← Back
+        </button>
+        <h1 className="text-md lg:text-2xl font-bold pb-4">{item.title}</h1>
+
+        <p className="mt-4 text-sm lg:text-lg">{item.snippet}</p>
+        <p className="pt-4">
+          <a
+            href={item.link}
+            target="_blank"
+            className="text-blue-600 underline "
+          >
+            Visit Link
+          </a>
+        </p>
+      </div>
+    </section>
   );
 };
 
